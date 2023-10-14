2c3d0e2c637153098cf2760217ede3bc73b90c9e
Max Weinzierl <supermaxman1@gmail.com>
2023-10-13 22:00:24 -0500

    Added scripts

--- scripts/vicuna-13b.sh
+++ scripts/vicuna-13b.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# python code/articulate.py --api replicate --api_key $REPLICATE_API_TOKEN --method few --model vicuna-13b
+python code/articulate.py --api replicate --api_key $REPLICATE_API_TOKEN --method few --model vicuna-13b
 python code/relations.py --api replicate --api_key $REPLICATE_API_TOKEN --method few --model vicuna-13b
 python code/articulate.py --api replicate --api_key $REPLICATE_API_TOKEN --method iccl --model vicuna-13b
 python code/relations.py --api replicate --api_key $REPLICATE_API_TOKEN --method iccl --model vicuna-13b
